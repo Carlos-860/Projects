1ff3791863c5f27e2e7fb902aeba1ecd1b461736
Carlos Timotheus <carlostimotheus5@gmail.com>
2021-11-18 12:07:29 +0200

    Phase 2 Completed

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,5 +1,10 @@
+// import Native React
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Import App Component, Stylesheet, google fonts
 import App from './App.js';
+import './style.css';
+// import '';
 
 ReactDOM.render(<App />, document.getElementById('root'));
