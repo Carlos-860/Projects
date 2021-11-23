ce8f0e6089dd6f736934f17e8ba9ef3dda56564c
Carlos Timotheus <carlostimotheus5@gmail.com>
2021-11-23 18:39:48 +0200

    Todos App - Phase 4

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,29 @@
 import TodoItem from './components/TodoItem';
-import TodoData from './todoData';
+import todosData from './todoData';
 
-function App() {
-  const todoItems = TodoData.map((item) => <TodoItem key={item.id} item={item} />);
+// function App() {
+//   const todoItems = TodoData.map((item) => <TodoItem key={item.id} item={item} />);
 
-  return <div className="todo-list">{todoItems}</div>;
+//   return <div className="todo-list">{todoItems}</div>;
+// }
+
+// export default App;
+
+import React from 'react';
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todos: todosData,
+    };
+  }
+
+  render() {
+    const todoItems = this.state.todos.map((item) => <TodoItem key={item.id} item={item} />);
+
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 
 export default App;
