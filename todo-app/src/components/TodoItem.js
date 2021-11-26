3a305d13aa68104127c39efcc3fa2fef3ae385ce
Carlos Timotheus <carlostimotheus5@gmail.com>
2021-11-26 15:10:08 +0200

    Todo App Phase 4

--- todo-app/src/components/TodoItem.js
+++ todo-app/src/components/TodoItem.js
@@ -1,7 +1,13 @@
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed} />
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onChange={() => {
+          console.log('Handling Event');
+        }}
+      />
       <p>{props.item.text}</p>
     </div>
   );
