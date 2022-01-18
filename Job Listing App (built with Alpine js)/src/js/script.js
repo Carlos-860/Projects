cefc1bcbd0d16f9198e4496723e785b9eb3772df
Carlos Timotheus <carlostimotheus5@gmail.com>
2022-01-18 21:26:21 +0200

    Job Listing App Intitial Commit

---
+++ Job Listing App (built with Alpine js)/src/js/script.js
@@ -0,0 +1,38 @@
+function jobApp() {
+  return {
+    jobList: [
+      {
+        id: '1',
+        title: 'Backend Developer',
+        type: 'Full-time',
+        location: 'Remote',
+        category: 'Engineering',
+      },
+
+      {
+        id: '2',
+        title: 'User Interface Designer',
+        type: 'Part-time',
+        location: 'Remote',
+        category: 'Design',
+      },
+
+      {
+        id: '3',
+        title: 'SEO Specialist',
+        type: 'Full-time',
+        location: 'USA',
+        category: 'SEO',
+      },
+
+      {
+        id: '4',
+        title: 'Content Writer',
+        type: 'Part-time',
+        location: 'Netherlands',
+        category: 'Writing',
+      },
+    ],
+    atTop: false,
+  };
+}
