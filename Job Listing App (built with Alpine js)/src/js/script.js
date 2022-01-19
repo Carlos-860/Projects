2fd7b87d6b6ca86c28d6a6a2bdd8304c953b0757
Carlos Timotheus <carlostimotheus5@gmail.com>
2022-01-19 22:55:27 +0200

    Local storage Functionailty added to application

--- Job Listing App (built with Alpine js)/src/js/script.js
+++ Job Listing App (built with Alpine js)/src/js/script.js
@@ -1,6 +1,6 @@
 function jobApp() {
   return {
-    jobList: [
+    jobList: $persist([
       {
         id: '1',
         title: 'Backend Developer',
@@ -29,10 +29,58 @@ function jobApp() {
         id: '4',
         title: 'Content Writer',
         type: 'Part-time',
-        location: 'Netherlands',
+        location: 'Hybrid',
         category: 'Writing',
       },
-    ],
+    ]),
+
+    activeJob: {
+      title: 'Web Developer',
+      type: 'Full-time',
+      location: 'Remote',
+      category: 'Engineering',
+    },
+
     atTop: false,
+    newPostModal: false,
+    editMode: false, // To ensure where change heading of modal
+
+    addNewJob() {
+      this.activeJob.id = 5;
+      this.jobList.push(this.activeJob);
+
+      this.newPostModal = false;
+      this.activeJob.id++;
+      console.log(this.activeJob);
+    },
+
+    editJob(id) {
+      this.editMode = true;
+      this.newPostModal = true;
+      this.activeJob = this.jobList.find((item) => {
+        return item.id == id;
+      });
+    },
+
+    updateJob(id) {
+      var getJob = this.jobList.find((item) => {
+        return item.id == id;
+      });
+
+      if (getJob) {
+        this.jobList.splice(this.jobList.indexOf(getJob), 1, this.activeJob);
+      }
+
+      this.newPostModal = false;
+    },
+
+    removeJob(id) {
+      var find = this.jobList.find((item) => {
+        return item.id === id;
+      });
+      if (find) {
+        this.jobList.splice(this.jobList.indexOf(find), 1);
+      }
+    },
   };
 }
